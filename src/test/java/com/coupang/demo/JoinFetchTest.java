cb2d532b5752e99d899ddd89092c652ea94322ce
changgunyee <minyoung403@naver.com>
2020-12-31 00:50:49 +0900

    left join fetch 테스트

--- src/test/java/com/coupang/demo/JoinFetchTest.java
+++ src/test/java/com/coupang/demo/JoinFetchTest.java
@@ -1,6 +1,7 @@
 package com.coupang.demo;
 
 import com.coupang.demo.entity.Item;
+import com.coupang.demo.entity.OutboundShipment;
 import com.coupang.demo.entity.VendorItem;
 import com.coupang.demo.repository.ItemRepository;
 import com.coupang.demo.repository.OutboundShipmentRepository;
@@ -13,8 +14,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @RunWith(SpringRunner.class)
@@ -47,4 +51,15 @@ public class JoinFetchTest {
         }).hasMessageContaining("object references an unsaved transient instance");
     }
 
+    @Test
+    @DisplayName("left join fetch 쓰면 left outer join으로 가져옴")
+    void leftJoinFetch() {
+        Item item = itemRepository.save(new Item());
+        VendorItem vendorItem = vendorItemRepository.save(VendorItem.builder().itemList(Arrays.asList(item)).build());
+        OutboundShipment outboundShipment1 = outboundShipmentRepository.save(OutboundShipment.builder().orderNumber(1L).vendorItemList(Arrays.asList(vendorItem)).build());
+        OutboundShipment outboundShipment2 = outboundShipmentRepository.save(OutboundShipment.builder().orderNumber(1L).build());
+
+        List<OutboundShipment> outboundShipments = outboundShipmentRepository.findByOrderNumber(1L);
+        assertThat(outboundShipments).hasSize(2);
+    }
 }
